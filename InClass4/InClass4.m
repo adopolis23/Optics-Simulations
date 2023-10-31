bda420c71557ac7b2dbf773d6e94441884fc6607
Adopolis23 <brandonw406@gmail.com>
2023-10-30 22:35:26 -0400

    In class 4 complete

--- InClass4/InClass4.m
+++ InClass4/InClass4.m
@@ -18,3 +18,16 @@ deltaOD8 = log(Amp830/amp8_base)
 
 deltaODM = [deltaOD6 deltaOD7 deltaOD8]
 
+deltaMUA = deltaODM / 6
+
+delta_C = linsolve(E, deltaMUA')
+
+HBO_vec = delta_C(1,:)'
+HBR_vec = delta_C(2,:)'
+
+figure
+%subplot(3,1,[1 2])
+plot(TimeAxis,HBO_vec,'b',TimeAxis,HBR_vec,'r');
+xlabel('Time');
+ylabel('Relative Concentration');
+legend('HBO','HBR');
